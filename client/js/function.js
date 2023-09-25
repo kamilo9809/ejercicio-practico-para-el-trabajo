53a9657a6e89969dda615cac0b095d5ab7a56554
kamilo9809 <89549994+kamilo9809@users.noreply.github.com>
2023-09-25 01:47:55 -0500

    actualizacion
    actualizacion menu de user

--- client/js/function.js
+++ client/js/function.js
@@ -1,1 +1,21 @@
-//
+// Obtener los elementos por su id
+var menulateral = document.querySelector("#menulateral");
+var userMenu = document.querySelector("#userMenu");
+
+// Agregar evento de clic al elemento menulateral
+menulateral.addEventListener("click", function() {
+    // Cambiar la clase para mostrar el elemento userMenu
+    userMenu.classList.remove("d-none");
+    userMenu.classList.add("d-block");
+    menulateral.classList.add("d-none");
+    // Crear el elemento de botón
+    const closeButton = document.createElement("button");
+    closeButton.innerHTML = "❌";
+    closeButton.classList.add("close");
+    closeButton.classList.add("w-100")
+  
+    // Agregar el botón como hijo del elemento userMenu
+    userMenu.insertBefore(closeButton, userMenu.firstChild);
+});
+
+
